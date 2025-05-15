c95e5d8bfaab169cd2c3ffabc85625ca616683f1
muralikrishna188 <muralid188@outlook.com>
2025-05-15 14:30:31 +0530

    test

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "Amazon-Mobile-Dev-RG1"
+    storage_account_name = "amazonmobiledevstorage"
+    container_name = "prodconfig"
+    key = "terraform.prodconfig"
+    access_key = "L7P0+jjRQa8HQNOZae+7GwRxhu0wAWX/I4dErFVbJEqcSq/YPV3stF01VtipF1T4qpmN9FqFOiId+AStL08I2A=="
+    
+  }
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.27.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {
+      
+    }
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
+
+  
+}
+locals {
+  setup_name = "amazon-hyd"
+}
+resource "azurerm_resource_group" "azapprg101" {
+    name = "azureapprg"
+    location = "central us"
+    tags = {
+      "name" = "${local.setup_name}-rsg"
+    }
+  
+}
+resource "azurerm_service_plan" "azappplan101" {
+    name = "devazappplan"
+    resource_group_name = azurerm_resource_group.azapprg101.name
+    location = azurerm_resource_group.azapprg101.location
+    sku_name = "S1"
+    os_type = "Windows"
+    tags = {
+      "name" = "${local.setup_name}-appplan"
+    }
+  
+}
+resource "azurerm_windows_web_app" "azwebapp101" {
+    name = "mywebapp02456"
+    resource_group_name = azurerm_resource_group.azapprg101.name
+    location = azurerm_resource_group.azapprg101.location
+    service_plan_id = azurerm_service_plan.azappplan101.id
+    depends_on = [ azurerm_service_plan.azappplan101 ]
+    site_config {
+      
+    }
+    tags = {
+      "name" = "${local.setup_name}-webapp"
+    }
+  
+}
